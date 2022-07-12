442c83eaece613711fa110702304ade339c1fa77
moey920 <moey920@musma.net>
2022-07-12 11:50:39 +0900

    fix: tf_iris_model/data_loader ssl

--- kubeflow/tf_iris_model/src/data_loader.py
+++ kubeflow/tf_iris_model/src/data_loader.py
@@ -1,6 +1,18 @@
 import tensorflow as tf
 import pandas as pd
 import os
+import requests
+requests.packages.urllib3.disable_warnings()
+import ssl
+
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 
 def data_loader():
